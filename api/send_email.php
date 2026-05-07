21d870be12253fca67bdf4cc7f7099db9a88f605
freshmediainternational-dev <freshmediainternational@gmail.com>
2026-05-07 13:49:08 +0100

    added admin protection

--- api/send_email.php
+++ api/send_email.php
@@ -1,47 +1,29 @@
 <?php
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-
-require 'PHPMailer/src/Exception.php';
-require 'PHPMailer/src/PHPMailer.php';
-require 'PHPMailer/src/SMTP.php';
-
 function sendVerificationEmail($to_email, $to_name, $code){
-    $mail = new PHPMailer(true);
+    $subject = "Your Taskgram Verification Code";
 
-    try {
-        $mail->isSMTP();
-        $mail->Host = 'smtp.gmail.com';
-        $mail->SMTPAuth = true;
-        $mail->Username = 'damilareisrael53@gmail.com@gmail.com';
-        $mail->Password = 'rece ihal szhz pedb';
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-        $mail->Port = 587;
+    $message ="
+    <html>
+    <body style='font-family:Arial,sans-serif;max-width:500px;margin:0 auth;padding:30px;'>
+    <h2 style='color:#6c63ff;'>Taskgram Verification</h2>
+    <p>Hi $to_name.</p>
+    <p>Your verification code is:</p>
+    <div style='background:#f8f7ff;padding:20px;border-radius:10px;text-align:center;margin20px 0;'>
+    <h1 style='color:#6c63ff;font-size:400px;letter-spacing:10px;'>$code</h1>
+    </div>
+    <p>This code expires in <strong>10 minutes</strong>.</p>
+    <p>If you didn't request this, please ignore this email.</p>
+    <br>
+    <p>The Taskgram Team</p>
+</body>
+</html>
+";
 
-        $mail->setFrom('damilareisrael53@gmail.com', 'Taskgram');
-        $mail->addAddress($to_email, $to_name);
+$headers = "MIME-VERSION: 1.0" . "\r\n";
+$headers = "Content-type:text/html;chartset=UTF-8" . "\r\n";
+$headers = "From: Taskgram <noreply@taskgram.infinityfreeapp.com>" . "\r\n";
 
-        $mail->isHTML(true);
-        $mail->Subject = 'Your Taskgram Verification Code';
-        $mail->Body = "
-            <div style='font-family:Arial,sans-serif;max-width:500px;margin:0 auto;padding:30px;'>
-                <h2 style='color:#6c63ff;'>Taskgram Verification</h2>
-                <p>Hi $to_name,</p>
-                <p>Your verification code is:</p>
-                <div style='background:#f8f7ff;padding:20px;border-radius:10px;text-align:center;margin:20px 0;'>
-                    <h1 style='color:#6c63ff;font-size:40px;letter-spacing:10px;'>$code</h1>
-                </div>
-                <p>This code expires in <strong>10 minutes</strong>.</p>
-                <p>If you didn't request this, please ignore this email.</p>
-                <br>
-                <p>The Taskgram Team</p>
-            </div>
-        ";
-
-        $mail->send();
-        return true;
-    } catch (Exception $e) {
-        return false;
-    }
+$sent =mail($to_email, $subject, $message, $headers);
+return $sent;
 }
 ?>
